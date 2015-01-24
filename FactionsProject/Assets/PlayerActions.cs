438e0a767b97eb838c1c2d97ec8f0de6736428f2
Ryan Anderson <esrever.nayr@gmail.com>
2015-01-23 23:30:43 -0800

    start project, basic input

---
+++ FactionsProject/Assets/PlayerActions.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerActions : MonoBehaviour {
+	
+	public Vector3 targetPos;
+	public float speed;
+	
+	// Use this for initialization
+	void start() {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 100000)) { 
+				targetPos = ray.GetPoint(0); 
+				targetPos.z = 0;
+				transform.position = targetPos; 
+			}
+		}
+
+		if (targetPos.z < 0) {
+			transform.Translate(speed * Input.GetAxis ("Horizontal"),
+			                    speed * Input.GetAxis ("Vertical"),
+			                    0);
+		} else {
+
+		}
+	}
+}
+

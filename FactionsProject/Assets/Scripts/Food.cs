210d2f05d257f4daaaa37c47ebd655f160c93c13
Ryan Anderson <esrever.nayr@gmail.com>
2015-01-24 19:51:50 -0800

    Log food pickup event

--- FactionsProject/Assets/Scripts/Food.cs
+++ FactionsProject/Assets/Scripts/Food.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class Food : MonoBehaviour {
 
+	private Transform player;
+	public string state;
+
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		state = "ground";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if ("ground" == state) {
+			float distance = Vector3.Distance(transform.position, player.position);
+			if (distance <= 1) {
+				state = "pickup";
+				Debug.Log ("Food.update: pickup");
+			}
+		}
+		else if ("pickup" == state) {
+			// TODO
+		}
 	}
 }

2d0f1315742d1dc442d7b0731a1636861f48291c
Ryan Anderson <esrever.nayr@gmail.com>
2015-01-24 18:50:53 -0800

    food spawner spawns food

---
+++ FactionsProject/Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodSpawner : MonoBehaviour {
+	
+	public int faction;
+	private int spawnrate;
+	public GameObject prototype;
+
+	// Use this for initialization
+	void Start () {
+		SpawnUnit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SpawnUnit() {
+		Instantiate(prototype, transform.position, transform.rotation);
+	}
+}
